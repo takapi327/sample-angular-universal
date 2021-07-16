f649abd1c6d6f0bc54f53078fbd06125ef94b901
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-07-16 23:38:09 +0900

    Create SSRRequestInterceptor

---
+++ src/lib/router/ssr-request-interceptor.ts
@@ -0,0 +1,29 @@
+import { Injectable, Inject, Optional }                         from '@angular/core'
+import { HttpInterceptor, HttpHandler, HttpRequest, HttpEvent } from '@angular/common/http'
+
+import { Request }     from 'express'
+import { Observable }  from 'rxjs'
+import { REQUEST }     from '@nguniversal/express-engine/tokens'
+import { environment } from 'src/environments/environment'
+
+@Injectable()
+export class SSRRequestInterceptor implements HttpInterceptor{
+
+  constructor(
+    @Optional() @Inject(REQUEST) protected request: Request
+  ) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    let serverReq: HttpRequest<any> = req
+    if (this.request) {
+
+      let newUrl = environment.apiPath
+      if (!req.url.startsWith('/')) {
+        newUrl += '/'
+      }
+      newUrl += req.url
+      serverReq = req.clone({ url: newUrl })
+    }
+    return next.handle(serverReq)
+  }
+}
