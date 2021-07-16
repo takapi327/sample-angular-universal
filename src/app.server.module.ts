fc06e1201bd058948060694abbed2ae40272e762
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-07-16 23:41:09 +0900

    Set SSRRequestInterceptor

--- src/app.server.module.ts
+++ src/app.server.module.ts
@@ -1,8 +1,11 @@
-import { NgModule }     from '@angular/core'
-import { ServerModule } from '@angular/platform-server'
+import { NgModule }          from '@angular/core'
+import { HTTP_INTERCEPTORS } from '@angular/common/http'
+import { ServerModule }      from '@angular/platform-server'
 
 import { MainModule }   from './main.module'
 import { AppComponent } from './app'
+
+import { SSRRequestInterceptor } from './lib/router/'
 
 const MODULES = [
   MainModule,
@@ -12,5 +15,12 @@ const MODULES = [
 @NgModule({
   imports:    MODULES,
   bootstrap: [AppComponent],
+  providers: [
+    {
+      provide:  HTTP_INTERCEPTORS,
+      useClass: SSRRequestInterceptor,
+      multi:    true
+    }
+  ]
 })
 export class AppServerModule {}
